ef8bfa1375be0d0cf62dd36ccaf349a36ab0a4d9
Aivaras <aivaras.silale@gmail.com>
2017-05-05 14:27:54 +0200

    added not working GEM object

---
+++ CondFormats/DataRecord/src/GEMPedestalsRcd.cc
@@ -0,0 +1,15 @@
+// -*- C++ -*-
+//
+// Package:     CondFormats/DataRecord
+// Class  :     GEMPedestalsRcd
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Author:      Aivaras Silale
+// Created:     Fri, 05 May 2017 12:25:41 GMT
+
+#include "CondFormats/DataRecord/interface/GEMPedestalsRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+
+EVENTSETUP_RECORD_REG(GEMPedestalsRcd);
